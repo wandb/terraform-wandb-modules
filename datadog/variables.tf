b57b0756044811465e62626b80c50c18f6cc27c5
George Scott <george.scott@wandb.com>
2023-09-29 10:29:40 -0500

    working

---
+++ datadog/variables.tf
@@ -0,0 +1,40 @@
+variable "cluster_name" {
+  nullable = false
+  type     = string
+}
+
+variable "api_key" {
+  nullable = false
+  type     = string
+}
+
+variable "app_key" {
+  nullable = false
+  type     = string
+}
+
+variable "site" {
+  nullable = false
+  type     = string
+}
+
+# variable "k8s_cluster_ca_certificate" {
+#   nullable = false
+#   type     = string
+# }
+
+# variable "k8s_host" {
+#   nullable = false
+#   type     = string
+# }
+
+# variable "k8s_token" {
+#   nullable = false
+#   type     = string
+# }
+
+variable "namespace" {
+  description = "The name prefix for all resources created."
+  nullable    = false
+  type        = string
+}
---
+++ open-telemetry/variables-k8s.tf
@@ -0,0 +1,14 @@
+variable "k8s_cluster_ca_certificate" {
+   nullable = false
+   type     = string
+}
+
+ variable "k8s_host" {
+   nullable = false
+   type     = string
+}
+
+ variable "k8s_token" {
+   nullable = false
+   type     = string
+}
