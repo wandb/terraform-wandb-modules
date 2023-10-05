8ade264f9c8493f5b1345f36e4de47623f5eba0b
George Scott <george.scott@wandb.com>
2023-10-05 13:14:27 -0500

    updates

--- datadog/variables.tf
+++ datadog/variables.tf
@@ -18,20 +18,20 @@ variable "site" {
   type     = string
 }
 
-# variable "k8s_cluster_ca_certificate" {
-#   nullable = false
-#   type     = string
-# }
+variable "k8s_cluster_ca_certificate" {
+  nullable = false
+  type     = string
+}
 
-# variable "k8s_host" {
-#   nullable = false
-#   type     = string
-# }
+variable "k8s_host" {
+  nullable = false
+  type     = string
+}
 
-# variable "k8s_token" {
-#   nullable = false
-#   type     = string
-# }
+variable "k8s_token" {
+  nullable = false
+  type     = string
+}
 
 variable "namespace" {
   description = "The name prefix for all resources created."
